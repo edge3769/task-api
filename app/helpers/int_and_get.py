f5a8f262234ca64efb30cca3bab6880becadc4e8
EdwardGold Edem Hogan <edge37@outlook.com>
2021-10-02 16:22:35 +0100

    haha

--- app/helpers/int_and_get.py
+++ app/helpers/int_and_get.py
@@ -2,6 +2,7 @@ from . import int_array
 from . import modelArray
 
 def int_and_get(ids, Model, name):
-    int_array(ids, name)
-    console.log('ids', ids)
-    modelArray(ids, Model, name)
+    if ids:
+        int_array(ids, name)
+        print('ids', ids)
+        modelArray(ids, Model, name)
